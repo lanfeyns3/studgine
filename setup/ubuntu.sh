3897771b23b502daaee6b158497c16efc73e5916
lanfeyns <211167187+lanfeyns3@users.noreply.github.com>
2025-06-10 04:40:30 +0000

    Added Ubuntu Based OS support

---
+++ setup/ubuntu.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo -------------------------- Downloading GNU --------------------------
+
+sudo apt install build-essential
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading Wayland --------------------------
+
+sudo apt install libwayland-dev
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading X11 --------------------------
+
+sudo apt install libx11-dev
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading XRandr --------------------------
+
+sudo apt install libxrandr-dev
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading Xinerama --------------------------
+
+sudo apt install libxinerama-dev
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading XCursor --------------------------
+
+sudo apt install libxcursor-dev
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading XI ---------------------------
+
+sudo apt install libxi-dev
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading PKG-Config --------------------------
+
+sudo apt install pkg-config
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading Xkbcommon --------------------------
+
+sudo apt install libxkbcommon-dev
+
+echo ------------------------------------------------------
+
+echo -------------------------- Downloading Mesa --------------------------
+
+sudo apt install mesa-common-dev
+
+echo -------------------------- Finished --------------------------
